ed4e4b9f3f531ec00786931541ba234446e9896f
Jeff Peterson <jeff@yak.sh>
2018-07-30 14:34:18 -0600

    Add hello world files

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,38 @@
 const path = require("path")
 const ChromeExtensionReloader = require("webpack-chrome-extension-reloader")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CleanWebpackPlugin = require("clean-webpack-plugin")
 
 module.exports = {
   mode: "development",
-  entry: "./src/main.js",
+  entry: "./src/main.tsx",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.bundle.js",
   },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
   plugins: [
+    new CleanWebpackPlugin(["dist"]),
     new ChromeExtensionReloader({
       port: 9090,
+    }),
+    new HtmlWebpackPlugin({
+      title: "Capstone",
     }),
   ],
 }
