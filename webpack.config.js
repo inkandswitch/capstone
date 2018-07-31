a020d7ad7386858fd621d1149cf0fc74ae50e425
Jeff Peterson <jeff@yak.sh>
2018-07-31 11:38:43 -0600

    Add file-loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
         use: "ts-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: "file-loader",
+      },
     ],
   },
   plugins: [
