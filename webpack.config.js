159951d121e0e55e37af244f68bb31c4f16bfe48
Jeff Peterson <jeff@yak.sh>
2018-08-17 09:37:08 -0600

    Add codemirror css

--- webpack.config.js
+++ webpack.config.js
@@ -29,11 +29,10 @@ module.exports = {
           "style-loader",
           {
             loader: "css-loader",
-            options: {
-              importLoaders: 1,
-            },
+            // options: {
+            //   importLoaders: 1,
+            // },
           },
-          "postcss-loader",
         ],
       },
       {
