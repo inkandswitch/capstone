bd7ddb92cda51e5df2aa8d4dcd391e70263446e7
Jeff Peterson <jeff@yak.sh>
2018-08-20 16:34:45 -0700

    Add Gesture component

---
+++ src/components/Gesture.tsx
@@ -0,0 +1,30 @@
+import * as Preact from "preact"
+import Handler from "./Handler"
+import * as Hammer from "hammerjs"
+
+interface Props {
+  onPinch?: (event: HammerInput) => void
+}
+
+export default class Gesture extends Handler<Props> {
+  hammer: HammerManager
+
+  componentDidMount() {
+    if (!this.base) return
+
+    const { onPinch } = this.props
+
+    const recognizers: RecognizerTuple[] = []
+
+    if (onPinch) recognizers.push([Hammer.Pinch, { threshold: 0.5 }])
+
+    this.hammer = new Hammer.Manager(this.base, {
+      recognizers,
+    })
+    this.hammer.on("pinch", this.handle("onPinch"))
+  }
+
+  render() {
+    return this.child
+  }
+}
---
+++ src/components/Handler.tsx
@@ -0,0 +1,44 @@
+import * as Preact from "preact"
+
+interface Event {
+  preventDefault?: Function
+  stopPropagation?: Function
+}
+
+interface Props {
+  children: JSX.Element
+  [name: string]: any
+}
+
+export default abstract class Handler<
+  InProps = {},
+  State = {}
+> extends Preact.Component<InProps & Props, State> {
+  handlers: { [name: string]: (event: Event) => void } = {}
+
+  get child(): JSX.Element {
+    const { children } = this.props
+    return Array.isArray(children) ? children[0] : null
+  }
+
+  filter(event: Event): Boolean {
+    return true
+  }
+
+  stop = (event: Event) => {
+    event.stopPropagation && event.stopPropagation()
+    event.preventDefault && event.preventDefault()
+  }
+
+  handle = (name: string) => {
+    return this.handlers[name] || (this.handlers[name] = this.handlerFor(name))
+  }
+
+  handlerFor(name: string) {
+    return (event: Event) => {
+      if (!this.props[name] || !this.filter(event)) return
+      this.stop(event)
+      this.props[name](event)
+    }
+  }
+}
