68e13803afc2409441ea854e279248537902a22e
Matt Tognetti <matt@shiftmessenger.com>
2018-08-20 21:30:21 -0700

    Fix CodeMirror focus issue.
    Prevent Draggable from clearing document-wide selections on drag end.

--- src/components/DraggableCard.tsx
+++ src/components/DraggableCard.tsx
@@ -26,7 +26,7 @@ export default class DraggableCard extends Preact.Component<Props> {
     } = this.props
 
     return (
-      <Draggable defaultPosition={{ x, y }} onStart={this.start} z={z}>
+      <Draggable defaultPosition={{ x, y }} onStart={this.start} z={z} enableUserSelectHack={false}>
         <Card>{children}</Card>
       </Draggable>
     )
