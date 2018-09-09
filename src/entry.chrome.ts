296752d0c2acae0c6382a17031d92cf23968c79c
Peter van Hardenberg <pvh@pvh.ca>
2018-09-09 12:14:56 -0700

    fixes for networking

--- src/entry.chrome.ts
+++ src/entry.chrome.ts
@@ -1,35 +1,38 @@
+process.hrtime = require("browser-process-hrtime")
 import StoreComms from "./data/StoreComms"
 
 let mainWindow: chrome.app.window.AppWindow
 
-chrome.app.runtime.onLaunched.addListener(() => {
-  chrome.app.window.create(
-    "index.html",
-    {
-      id: "main",
-      frame: "chrome",
-      state: "fullscreen",
-      resizable: true,
-      outerBounds: {
-        // Half-screen default for development
-        // Press "square" key (F3) to toggle
-        top: 0,
-        left: 0,
-        width: screen.width / 2,
-        height: screen.height,
+setTimeout(() => {
+  chrome.app.runtime.onLaunched.addListener(() => {
+    chrome.app.window.create(
+      "index.html",
+      {
+        id: "main",
+        frame: "chrome",
+        state: "fullscreen",
+        resizable: true,
+        outerBounds: {
+          // Half-screen default for development
+          // Press "square" key (F3) to toggle
+          top: 0,
+          left: 0,
+          width: screen.width / 2,
+          height: screen.height,
+        },
       },
-    },
-    win => {
-      mainWindow = win
-      chrome.storage.local.get(["disableFullscreen"], result => {
-        if (!result.disableFullscreen) {
-          win.fullscreen()
-        }
-      })
-      win.show(true) // Passing focused: true
-    },
-  )
-})
+      win => {
+        mainWindow = win
+        chrome.storage.local.get(["disableFullscreen"], result => {
+          if (!result.disableFullscreen) {
+            win.fullscreen()
+          }
+        })
+        win.show(true) // Passing focused: true
+      },
+    )
+  })
+}, 1000)
 
 let comms = new StoreComms()
 
@@ -37,4 +40,6 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) =>
   comms.onMessage(request, sender, sendResponse),
 )
 
-chrome.runtime.onConnect.addListener(port => comms.onConnect(port))
+comms.hypermerge.ready.then(() => {
+  chrome.runtime.onConnect.addListener(port => comms.onConnect(port))
+})
