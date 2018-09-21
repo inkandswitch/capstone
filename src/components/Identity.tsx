b4994514567663c61da1413cb38756ec4be58136
Matt Tognetti <matt@shiftmessenger.com>
2018-09-21 13:21:28 -0700

    Avatar and name input

--- src/components/Identity.tsx
+++ src/components/Identity.tsx
@@ -3,11 +3,13 @@ import Content, { DocumentActor } from "./Content"
 import { ShelfContents, ShelfContentsRequested } from "./Shelf"
 import StrokeRecognizer, { GlyphEvent, Glyph } from "./StrokeRecognizer"
 import * as Reify from "../data/Reify"
+import * as Link from "../data/Link"
 import * as Widget from "./Widget"
 import { AnyDoc } from "automerge/frontend"
 
 interface Model {
   name: string
+  avatarUrl: string
   documents: string[]
 }
 
@@ -19,13 +21,25 @@ export class IdentityActor extends DocumentActor<Model, InMessage, OutMessage> {
   async onMessage(message: InMessage) {
     switch (message.type) {
       case "ShelfContentsRequested": {
-        this.emit({ type: "ShelfContentsRequested" })
+        this.emit({ type: "ShelfContentsRequested", body: message.body })
         break
       }
       case "ShelfContents": {
-        const { urls } = message.body
+        const { urls, intent } = message.body
+        if (!urls.length) return
+
         this.change(doc => {
-          doc.documents = doc.documents.concat(urls)
+          if (intent === "avatar") {
+            // Only change avatar if there is a single document on the shelf and it is
+            // an image.
+            // TODO: Feedback if this fails
+            const { type } = Link.parse(urls[0])
+            if (urls.length === 1 && type === "Image") {
+              doc.avatarUrl = urls[0]
+            }
+          } else {
+            doc.documents = doc.documents.concat(urls)
+          }
           return doc
         })
         break
@@ -35,23 +49,56 @@ export class IdentityActor extends DocumentActor<Model, InMessage, OutMessage> {
 }
 
 interface Props extends Widget.Props<Model, WidgetMessage> {}
+interface State {
+  isEditing: boolean
+}
 
-export class Identity extends Preact.Component<Props> {
+export class Identity extends Preact.Component<Props, State> {
+  state = { isEditing: false }
+
   static reify(doc: AnyDoc): Model {
     return {
       name: Reify.string(doc.name),
+      avatarUrl: Reify.string(doc.avatarUrl),
       documents: Reify.array(doc.documents),
     }
   }
 
-  onGlyph = (stroke: GlyphEvent) => {
+  onPointerDown = (event: PointerEvent) => {
+    console.log("onTap")
+    if (this.state.isEditing) this.setState({ isEditing: false })
+  }
+
+  onBadgePointerDown = (event: PointerEvent) => {
+    if (this.state.isEditing) event.stopPropagation()
+  }
+
+  onBadgeGlyph = (stroke: GlyphEvent) => {
     switch (stroke.glyph) {
       case Glyph.paste:
-        this.props.emit({ type: "ShelfContentsRequested" })
+        this.props.emit({
+          type: "ShelfContentsRequested",
+          body: { intent: "avatar" },
+        })
+        break
+      case Glyph.edit:
+        this.setState({ isEditing: true })
+    }
+  }
+
+  onCubbyGlyph = (stroke: GlyphEvent) => {
+    switch (stroke.glyph) {
+      case Glyph.paste:
+        this.props.emit({
+          type: "ShelfContentsRequested",
+          body: { intent: "cubby" },
+        })
+        break
     }
   }
 
   onChange = (event: any) => {
+    console.log("CHANGE")
     this.props.change(doc => {
       doc.name = event.target.value
       return doc
@@ -70,18 +117,23 @@ export class Identity extends Preact.Component<Props> {
   }
 
   renderFullscreen() {
-    const { name, documents } = this.props.doc
+    const { name, avatarUrl, documents } = this.props.doc
+    const { isEditing } = this.state
     return (
-      <StrokeRecognizer onGlyph={this.onGlyph}>
-        <div style={style.Identity}>
+      <div style={style.Identity} onPointerDown={this.onPointerDown}>
+        <StrokeRecognizer onGlyph={this.onBadgeGlyph}>
           <div style={style.Profile}>
-            <input
-              style={style.NameInput}
-              onChange={this.onChange}
-              value={name}
-              placeholder={"Anonymous"}
-            />
+            <div onPointerDown={this.onBadgePointerDown}>
+              <IdentityBadge
+                name={name}
+                avatarUrl={avatarUrl}
+                isEditing={isEditing}
+                onChange={this.onChange}
+              />
+            </div>
           </div>
+        </StrokeRecognizer>
+        <StrokeRecognizer onGlyph={this.onCubbyGlyph}>
           <div style={style.Documents}>
             {documents.map(docUrl => (
               <div style={style.Item}>
@@ -91,16 +143,65 @@ export class Identity extends Preact.Component<Props> {
               </div>
             ))}
           </div>
-        </div>
-      </StrokeRecognizer>
+        </StrokeRecognizer>
+      </div>
     )
   }
 
   renderPreview() {
-    const { name } = this.props.doc
+    const { name, avatarUrl } = this.props.doc
     return (
       <div style={style.preview.Identity}>
-        <h2>{name || "Anonymous"}</h2>
+        <IdentityBadge avatarUrl={avatarUrl} name={name} />
+      </div>
+    )
+  }
+}
+
+interface IdentityBadgeProps {
+  avatarUrl: string
+  name: string
+  isEditing?: boolean
+  onChange?: (e: Event) => void
+}
+
+class IdentityBadge extends Preact.Component<IdentityBadgeProps> {
+  inputEl?: HTMLElement
+
+  componentDidMount() {
+    if (this.inputEl && this.props.isEditing) {
+      this.inputEl.focus()
+    }
+  }
+
+  componentDidUpdate(prevProps: IdentityBadgeProps) {
+    if (this.inputEl && this.props.isEditing && !prevProps.isEditing) {
+      this.inputEl.focus()
+    }
+  }
+
+  render() {
+    const {
+      avatarUrl,
+      name,
+      isEditing = false,
+      onChange = () => {},
+    } = this.props
+    return (
+      <div style={style.IdentityBadge}>
+        <div style={style.IdentityBadgeAvatar}>
+          <Content mode="embed" url={avatarUrl} />
+        </div>
+        <div style={style.IdentityBadgeName}>
+          <input
+            ref={el => (this.inputEl = el)}
+            readOnly={!isEditing}
+            style={style.NameInput}
+            onInput={onChange}
+            value={name}
+            placeholder={"Anonymous"}
+          />
+        </div>
       </div>
     )
   }
@@ -108,9 +209,23 @@ export class Identity extends Preact.Component<Props> {
 
 const style = {
   preview: {
-    Identity: {
-      padding: 20,
-    },
+    Identity: {},
+  },
+  IdentityBadge: {
+    padding: 10,
+    display: "inline-flex",
+    height: "100%",
+    alignItems: "center",
+  },
+  IdentityBadgeAvatar: {
+    height: 75,
+    width: 75,
+    border: "3px solid #333",
+    borderRadius: 999,
+    overflow: "hidden",
+  },
+  IdentityBadgeName: {
+    marginLeft: 10,
   },
   Identity: {
     display: "flex",
@@ -120,7 +235,6 @@ const style = {
   },
   Profile: {
     borderBottom: "1px solid #aaa",
-    padding: 20,
   },
   NameInput: {
     fontWeight: 700,
