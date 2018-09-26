1df52069262860006086cf39f3037908bd6bd4b5
Peter van Hardenberg <pvh@pvh.ca>
2018-09-26 09:51:36 -0700

    a better implementation

--- src/components/CommandFeedback.tsx
+++ src/components/CommandFeedback.tsx
@@ -3,28 +3,25 @@ import { delay } from "lodash"
 import { EventEmitter } from "events"
 
 interface FeedbackData {
-  message: String
+  message?: string
 }
 
 class ProviderSingleton extends EventEmitter {
-  constructor() {
-    super()
-  }
   feedback: FeedbackData[] = []
   add(message: string) {
     this.feedback.push({ message })
-    this.emit("feedback", message)
-    delay(() => this.removeText(), 1000)
+    this.emit("feedback", this.feedback[0])
+    delay(() => this.removeText(), 500)
   }
   removeText() {
-    this.feedback = []
-    this.emit("feedback", null)
+    this.feedback.shift()
+    this.emit("feedback", this.feedback[0])
   }
 }
 export let Provider = new ProviderSingleton()
 
 export class Renderer extends Preact.Component {
-  listener: Function
+  state = { message: null }
 
   componentWillMount() {
     Provider.addListener("feedback", this.handleChange)
@@ -34,12 +31,12 @@ export class Renderer extends Preact.Component {
     Provider.removeListener("feedback", this.handleChange)
   }
 
-  handleChange = (message: string) => this.setState({ message })
+  handleChange = ({ message = "" } = {}) => this.setState({ message })
 
   render(props: any, state: any) {
-    if (Provider.feedback.length == 0) {
+    if (!this.state.message) {
       return null
     }
-    return <div className="DebugMessage">{Provider.feedback[0].message}</div>
+    return <div className="DebugMessage">{this.state.message}</div>
   }
 }
