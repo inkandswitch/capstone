8a482d5bc3d50e32406354ebab3cd857c5e3fdf4
Jeff Peterson <jeff@yak.sh>
2018-10-05 08:57:14 -0600

    use debug log on server

--- src/modules/discovery-cloud/server.js
+++ src/modules/discovery-cloud/server.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 let expressWs = require('express-ws')(app);
+let log = require('debug')('discovery-cloud:server')
 
 function mergeUniq(base = [], add = [], remove = []) {
   return base.concat(add).reduce((acc,val) => acc.includes(val) || remove.includes(val) ? acc : acc.concat(val),[])
@@ -23,22 +24,22 @@ class DiscoveryCloudServer {
     if (this.peers[id]) {
       this.peers[id].send(JSON.stringify(message))
     } else {
-      console.log("error - trying to send to bad peer", id)
+      log("error - trying to send to bad peer", id)
     }
   }
 
   ifUnion(id1, id2, cb) {
-    console.log("If union",id1,id2)
+    log("If union",id1,id2)
     if (id1 != id2) {
-      console.log("ALL",this.peerKeys)
+      log("ALL",this.peerKeys)
       const k1 = this.peerKeys[id1] || []
-      console.log("k1",k1)
+      log("k1",k1)
       const k2 = this.peerKeys[id2] || []
-      console.log("k2",k2)
+      log("k2",k2)
       const union = k1.filter(val => k2.includes(val))
-      console.log("union",union)
+      log("union",union)
       if (union.length > 0) {
-        console.log("union!",id1,id2)
+        log("union!",id1,id2)
         cb(union)
       }
     }
@@ -55,22 +56,22 @@ class DiscoveryCloudServer {
 
   listen() {
     app.use(function (req, res, next) {
-      console.log("middleware", req.url)
+      log("middleware", req.url)
       return next();
     });
 
 
     app.get('/', (req, res, next) => {
-      console.log("get /");
+      log("get /");
       res.end();
     });
 
     app.ws('/discovery', (ws, req) => {
-      console.log("discovery connection")
+      log("discovery connection")
       let id = null
       ws.on('message', (data) => {
         // {id:id,join:[...],leave:[...]}
-        console.log("message",data);
+        log("message",data);
         const msg = JSON.parse(data)
         id = msg.id
         this.peers[id] = ws
@@ -94,7 +95,7 @@ class DiscoveryCloudServer {
       if (this.looking[key2]) {
         const other = this.looking[key2]
         delete this.looking[key2]
-        console.log("piping", key1)
+        log("piping", key1)
         other.pipe(ws).pipe(other)
       } else {
         this.looking[key1] = ws
@@ -105,7 +106,7 @@ class DiscoveryCloudServer {
     })
 
     app.listen(this.port, "0.0.0.0", (err) => {
-      console.log("Listening on port",this.port)
+      log("Listening on port",this.port)
     });
   }
 }
