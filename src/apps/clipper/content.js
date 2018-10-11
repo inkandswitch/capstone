6420ecb3186b42ef909aed755446b19df4279c71
Peter van Hardenberg <pvh@pvh.ca>
2018-10-11 20:30:28 +0000

    stable extension IDs

--- src/apps/clipper/content.js
+++ src/apps/clipper/content.js
@@ -1,15 +1,13 @@
-document.body.style.backgroundColor = "red"
+document.body.style.backgroundColor = "blue"
 
 import freezeDry from "freeze-dry"
 
 // The ID of the extension we want to talk to.
-var capstoneExtensionId = "oiemcadceddgdehdimlfdajbkdjcdkdb"
+var capstoneExtensionId = "dflegkhjkkcbbnknalnkddcmjpaimcdp"
 
 console.log("prefreeze")
 freezeDry(document, { addMetadata: true }).then(html => {
-  console.log("froze", html)
-  // Make a simple request:
   chrome.runtime.sendMessage(capstoneExtensionId, { html }, response => {
-    console.log("Capstone got the HTML: ", response)
+    console.log("Capstone got the HTML!")
   })
 })
