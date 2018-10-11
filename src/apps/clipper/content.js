f3b7865b487fc5731223d0838e1eecd518993d50
Peter van Hardenberg <pvh@pvh.ca>
2018-10-11 21:43:34 +0000

    stop blueing yourself

--- src/apps/clipper/content.js
+++ src/apps/clipper/content.js
@@ -1,13 +1,10 @@
-document.body.style.backgroundColor = "blue"
-
 import freezeDry from "freeze-dry"
 
 // The ID of the extension we want to talk to.
 var capstoneExtensionId = "dflegkhjkkcbbnknalnkddcmjpaimcdp"
 
-console.log("prefreeze")
 freezeDry(document, { addMetadata: true }).then(html => {
   chrome.runtime.sendMessage(capstoneExtensionId, { html }, response => {
-    console.log("Capstone got the HTML!")
+    console.log("Capstone appears to have received the HTML.")
   })
 })
