71f8ea8458088e367e2fed65cd64ba11f8703076
Peter van Hardenberg <pvh@pvh.ca>
2018-10-12 16:58:51 -0700

    keyboard shortcuts for clipper

--- src/apps/clipper/popup.js
+++ src/apps/clipper/popup.js
@@ -7,15 +7,23 @@ function init() {
 
   var input = document.getElementById("noteInput")
 
-  document.getElementById("note").addEventListener("click", () => {
-    console.log("note")
-    chrome.runtime.sendMessage(
-      capstoneExtensionId,
-      { contentType: "Text", content: input.value },
-      response => {
-        console.log("Capstone appears to have received the Text.")
-      },
-    )
+  document.getElementById("noteForm").addEventListener("submit", e => {
+    e.preventDefault()
+
+    const form = e.target
+    const content = form[0].value
+    if (content) {
+      chrome.runtime.sendMessage(
+        capstoneExtensionId,
+        { contentType: "Text", content },
+        response => {
+          console.log("Capstone appears to have received the Text.")
+        },
+      )
+    }
+
+    form.reset()
+    window.close()
   })
 }
 
