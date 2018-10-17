f32a1f707c5389fd3a0f4f4920716c70d739a526
Matt Tognetti <matt@shiftmessenger.com>
2018-10-16 20:45:25 -0500

    duh

--- src/logic/DragMetrics.ts
+++ src/logic/DragMetrics.ts
@@ -20,17 +20,13 @@ export const init = (e: Input = { x: 0, y: 0 }) => {
 }
 
 export const update = (measurements: Measurements, input: Input) => {
-  const incrementalDelta = {
-    x: input.x - measurements.position.x,
-    y: input.y - measurements.position.y,
-  }
   const position = {
-    x: measurements.position.x + incrementalDelta.x,
-    y: measurements.position.y + incrementalDelta.y,
+    x: input.x,
+    y: input.y,
   }
   const delta = {
-    x: measurements.position.x - measurements.startPosition.x,
-    y: measurements.position.y - measurements.startPosition.y,
+    x: position.x - measurements.startPosition.x,
+    y: position.y - measurements.startPosition.y,
   }
   const magnitude = Math.sqrt(delta.x ** 2 + delta.y ** 2)
   return {
