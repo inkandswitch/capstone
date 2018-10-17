512d190db3c0199ed80f7176a5edb5eb0ce005b5
Matt Tognetti <matt@shiftmessenger.com>
2018-10-16 17:55:40 -0700

    name clarity

--- src/logic/Dragger.ts
+++ src/logic/Dragger.ts
@@ -15,7 +15,7 @@ export type OnStopHandler = (x: number, y: number) => void
 
 export class Dragger {
   private metrics?: DragMetrics.Metrics
-  private position: Point
+  private origin: Point
   private onStart?: OnStartHandler
   private onDrag?: OnMoveHandler
   private onStop?: OnStopHandler
@@ -26,13 +26,13 @@ export class Dragger {
     this.onDrag = options.onDrag
     this.onStop = options.onStop
     this.node = options.node
-    this.position = options.position
+    this.origin = options.position
   }
 
   start(e: Point) {
     const dragPosition = DOM.getOffsetFromParent(e, this.node)
     this.metrics = DragMetrics.init(dragPosition)
-    this.onStart && this.onStart(this.position.x, this.position.y)
+    this.onStart && this.onStart(this.origin.x, this.origin.y)
   }
 
   drag(e: Point) {
@@ -40,20 +40,25 @@ export class Dragger {
 
     const dragPoint = DOM.getOffsetFromParent(e, this.node)
     this.metrics = DragMetrics.update(this.metrics, dragPoint)
-    const position = {
-      x: this.position.x + this.metrics.delta.x,
-      y: this.position.y + this.metrics.delta.y,
+    const translate = {
+      x: this.origin.x + this.metrics.delta.x,
+      y: this.origin.y + this.metrics.delta.y,
     }
-    this.onDrag && this.onDrag(position.x, position.y)
+    this.onDrag && this.onDrag(translate.x, translate.y)
+  }
+
+  setPosition(e: Point) {
+    this.origin = e
+    this.metrics = undefined
   }
 
   stop() {
     if (!this.metrics) throw new Error("Must call star() before stop()")
-    this.position = {
-      x: this.position.x + this.metrics.delta.x,
-      y: this.position.y + this.metrics.delta.y,
+    this.origin = {
+      x: this.origin.x + this.metrics.delta.x,
+      y: this.origin.y + this.metrics.delta.y,
     }
     this.metrics = undefined
-    this.onStop && this.onStop(this.position.x, this.position.y)
+    this.onStop && this.onStop(this.origin.x, this.origin.y)
   }
 }
