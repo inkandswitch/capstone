4e86d99b34bc88ce656c28b249bf249be4539e64
Julia Roggatz <jroggatz@kayak.com>
2018-10-18 13:35:57 +0200

    Hmm not so great

--- src/logic/SizeUtils.ts
+++ src/logic/SizeUtils.ts
@@ -3,7 +3,10 @@ import { AnyDoc } from "automerge/frontend"
 import * as Link from "../data/Link"
 // import * as css from "../styles/styles.css"
 
+const TEXT_CARD_PADDING = 15
 const DEFAULT_CARD_MAX_SIZE = { width: 400, height: 400 }
+const TEXT_MAX_WIDTH = DEFAULT_CARD_MAX_SIZE.width - 2 * TEXT_CARD_PADDING
+const TEXT_MAX_HEIGHT = DEFAULT_CARD_MAX_SIZE.height - 2 * TEXT_CARD_PADDING
 
 export function calculateInitialSize(url: string, doc: AnyDoc): Promise<Size> {
   return new Promise((resolve, reject) => {
@@ -17,7 +20,11 @@ export function calculateInitialSize(url: string, doc: AnyDoc): Promise<Size> {
           resolve({ width: 400, height: 400 })
         })
     } else if (type === "Text") {
-      resolve(getTextSize((doc.content as string[]).join("")))
+      const textSize = getTextSize((doc.content as string[]).join(""))
+      resolve({
+        width: textSize.width + 2 * TEXT_CARD_PADDING,
+        height: textSize.height + 2 * TEXT_CARD_PADDING,
+      })
     } else if (type === "Board") {
       resolve({ width: 300, height: 200 })
     } else {
@@ -40,14 +47,28 @@ function getImageSize(src: string): Promise<Size> {
 }
 
 function getTextSize(text: string): Size {
-  let size = CalculateSize.default(text, {
-    fontSize: `12`,
+  let options = {
+    fontSize: `14px`,
     font: "Roboto, Arial, Helvetica, sans-serif",
-    width: `${DEFAULT_CARD_MAX_SIZE.width - 20}`,
-  })
-  console.log(`text: ${text}`)
-  console.log(`size: ${size.height} /  ${size.width}`)
-  return { width: 300, height: 300 }
+  }
+  const unconstrainedSize = CalculateSize.default(text, options)
+  if (unconstrainedSize.width < TEXT_MAX_WIDTH) {
+    return {
+      width: unconstrainedSize.width,
+      height: Math.min(unconstrainedSize.height - 5, TEXT_MAX_HEIGHT),
+    }
+  } else {
+    const constrainedSize = CalculateSize.default(
+      text,
+      Object.assign(options, {
+        width: `${TEXT_MAX_WIDTH}px`,
+      }),
+    )
+    return {
+      width: TEXT_MAX_WIDTH,
+      height: Math.min(constrainedSize.height, TEXT_MAX_HEIGHT),
+    }
+  }
 }
 
 function resolvedCardSize(originalSize: Size): Size {
