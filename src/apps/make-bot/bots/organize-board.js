dd414818b95e220bec2866a1647648692a0e63e1
Szymon Kaliski <hi@szymonkaliski.com>
2018-10-23 17:23:41 +0200

    simple pinterest-like board cleanup bot

---
+++ src/apps/make-bot/bots/organize-board.js
@@ -0,0 +1,39 @@
+const last = arr => arr[arr.length - 1]
+
+const MARGIN = 20
+const WINDOW_HEIGHT = window.innerHeight
+
+Content.open(Content.workspaceUrl, workspace => {
+  const boardUrl =
+    workspace.navStack.length > 0 ? last(workspace.navStack) : workspace.rootUrl
+
+  const changeBoard = Content.open(boardUrl, () => {})
+
+  changeBoard(doc => {
+    const avgWidth =
+      Object.values(doc.cards).reduce((memo, card) => card.width + memo, 0) /
+      Object.keys(doc.cards).length
+
+    let column = 0
+    let topOffset = 0
+
+    Object.keys(doc.cards).forEach(key => {
+      const card = doc.cards[key]
+
+      const aspect = card.width / card.height
+
+      card.width = avgWidth
+      card.height = avgWidth / aspect
+
+      if (topOffset + card.height + MARGIN * 2 > WINDOW_HEIGHT) {
+        column++
+        topOffset = 0
+      }
+
+      card.x = column * (avgWidth + MARGIN) + MARGIN
+      card.y = topOffset + MARGIN
+
+      topOffset = card.y + card.height
+    })
+  })
+})
