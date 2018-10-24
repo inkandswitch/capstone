bdbe233d856aa7fc6d782d0046a970f683b03324
Szymon Kaliski <hi@szymonkaliski.com>
2018-10-24 12:33:41 +0200

    updates for recent master rebase

--- src/apps/make-bot/make-bot.ts
+++ src/apps/make-bot/make-bot.ts
@@ -13,6 +13,15 @@ if (!workspace || !botId || !fileName || !fs.existsSync(fileName)) {
 
 const code = fs.readFileSync(fileName, "utf-8")
 
+import { LocalStorage } from "node-localstorage"
+
+interface Global {
+  localStorage: LocalStorage
+}
+declare var global: Global
+
+global.localStorage = new LocalStorage("./localstorage")
+
 const raf = require("random-access-file")
 import { Doc } from "automerge/frontend"
 import { last, once } from "lodash"
@@ -25,7 +34,7 @@ import CloudClient from "../../modules/discovery-cloud/Client"
 import { FrontendHandle } from "../../modules/hypermerge/frontend"
 import { Hypermerge } from "../../modules/hypermerge"
 
-import "../../components/Bot"
+import "./Bot" // we have local bot implementation since the Capstone one uses css imports
 import * as Board from "../../components/Board"
 import * as DataImport from "../../components/DataImport"
 import * as Workspace from "../../components/Workspace"
@@ -48,60 +57,79 @@ hm.joinSwarm(
 )
 
 hm.ready.then(hm => {
-  Content.open(workspace, (workspace: Doc<Workspace.Model>) => {
-    const boardUrl =
-      workspace.navStack.length > 0
-        ? last(workspace.navStack)
-        : workspace.rootUrl
+  Content.open(
+    workspace,
+    once((workspace: Doc<Workspace.Model>) => {
+      const boardUrl =
+        workspace.navStack.length > 0
+          ? last(workspace.navStack)
+          : workspace.rootUrl
 
-    if (!boardUrl) {
-      console.log("Can't find a board, exiting...")
-      return
-    }
-
-    console.log(`Using board: ${boardUrl}`)
-
-    const boardHandle = Content.open(boardUrl, (doc: any) => {
-      const botExists = !!doc.cards[botId]
-
-      // console.log("board doc", doc)
-
-      if (botExists) {
-        // update
-        const botHandle = Content.open(doc.cards[botId].url, (doc: any) => {
-          // console.log("bot doc", doc)
-        })
-
-        botHandle(bot => {
-          bot.code = code
-        })
-      } else {
-        // create
-        const botUrl = Content.create("Bot")
-
-        const botHandle = Content.open(botUrl, (doc: any) => {
-          // console.log("bot doc", doc)
-        })
-
-        botHandle(doc => {
-          doc.id = botId
-          doc.code = code
-        })
-
-        boardHandle((board: Doc<Board.Model>) => {
-          const card = {
-            id: botId,
-            z: 0,
-            x: 0,
-            y: 0,
-            width: 200,
-            height: 200,
-            url: botUrl,
-          }
-
-          board.cards[botId] = card
-        })
+      if (!boardUrl) {
+        console.log("Can't find a board, exiting...")
+        return
       }
-    })
-  })
+
+      console.log(`Using board: ${boardUrl}`)
+
+      const boardHandle = Content.open(
+        boardUrl,
+        once((doc: any) => {
+          const botExists = !!doc.cards[botId]
+
+          console.log("board doc", doc)
+
+          if (botExists) {
+            console.log("updating...")
+
+            // update
+            const botHandle = Content.open(
+              doc.cards[botId].url,
+              once((doc: any) => {
+                console.log("bot doc", doc)
+              }),
+            )
+
+            botHandle(bot => {
+              bot.code = code
+              setTimeout(() => process.exit(0), 10)
+            })
+          } else {
+            console.log("creating...")
+
+            // create
+            const botUrl = Content.create("Bot")
+
+            const botHandle = Content.open(
+              botUrl,
+              once((doc: any) => {
+                console.log("bot doc", doc)
+              }),
+            )
+
+            botHandle(doc => {
+              doc.id = botId
+              doc.code = code
+            })
+
+            boardHandle((board: Doc<Board.Model>) => {
+              const card = {
+                id: botId,
+                z: 0,
+                x: 0,
+                y: 0,
+                width: 200,
+                height: 200,
+                url: botUrl,
+              }
+
+              board.cards[botId] = card
+
+              setTimeout(() => process.exit(0), 10)
+            })
+          }
+        }),
+      )
+    }),
+  )
 })
