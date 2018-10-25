7bacfc1c5e19f80d2c2e075181b81b643dec347a
Orion Henry <orion.henry@gmail.com>
2018-10-25 19:51:48 -0400

    syncQueue

--- src/data/Queue.ts
+++ src/data/Queue.ts
@@ -2,21 +2,19 @@ import * as Debug from "debug"
 
 export default class Queue<T> {
   queue: T[] = []
-  subscription?: (item: T) => void
+  subscription?: (item: T, done: () => void) => void
   log: Debug.IDebugger
+  locked: boolean = false
+  push: (item: T) => void
 
   constructor(name: string = "unknown") {
     this.log = Debug(`queue:${name}`)
+    this.push = this.enqueue
   }
 
-  push(item: T) {
-    if (this.subscription) {
-      this.log("subbed", item)
-      this.subscription(item)
-    } else {
-      this.log("queued", item)
-      this.queue.push(item)
-    }
+  enqueue = (item: T) : void => {
+    this.log("queued", item)
+    this.queue.push(item)
   }
 
   subscribe(subscriber: (item: T) => void) {
@@ -26,15 +24,47 @@ export default class Queue<T> {
 
     this.log("subscribe")
 
+    while (this.queue.length > 0) {
+      const item = this.queue.shift()
+      subscriber(item!)
+    }
+
+    this.push = subscriber
+  }
+
+  subscribeSync(subscriber: (item: T, done: () => void) => void) {
+    if (this.subscription) {
+      throw new Error("only one subscriber at a time to a queue")
+    }
+
+    this.log("subscribeSync")
+
     this.subscription = subscriber
-    this.queue.forEach(e => {
-      subscriber(e)
-    })
-    this.queue = []
+
+    this.push = this.pushSync
+
+    this.shift()
+  }
+
+  private pushSync = (item: T) : void => {
+    this.enqueue(item)
+    if (!this.locked) this.shift()
+  }
+
+  private shift = () : void => {
+    if (this.queue.length > 0 && this.subscription) {
+      let item = this.queue.shift()
+      this.log("shifted", item)
+      this.locked = true
+      this.subscription(item!, this.shift)
+    } else {
+      this.locked = false
+    }
   }
 
   unsubscribe() {
     this.log("unsubscribe")
     this.subscription = undefined
+    this.push = this.enqueue
   }
 }
